0562d3b9f6e542a1cbc9166d6433b776a9503cc6
ruandqm <ruandqm@gmail.com>
2023-07-06 16:43:07 -0300

    axios add on api request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
+import axios from 'axios'
+
 export async function api(params: string) {
     try {
-        const response = await fetch(`https://newsapi.org/v2/top-headlines${params}&apiKey=93fd5f5228654cdabd24534ee8f0ce4b`)
-        return response.json()
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines${params}&apiKey=93fd5f5228654cdabd24534ee8f0ce4b`)
+
+        return response.data
     } catch (error) {
         console.log(error)
         throw error
